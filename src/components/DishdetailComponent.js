08611ee52f62f9e948db70baf3e1fa6ab96ca50f
Latikesh Nikam <latikesh2121@gmail.com>
2020-05-16 10:27:05 +0530

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,8 +27,8 @@ class CommentForm extends Component{
   }
 
   handleSubmit(values){
-    console.log("Current state is:" + JSON.stringify(values));
-    alert("Current state is:" + JSON.stringify(values))
+    this.toggleModal();
+    this.props.addComment(this.props.dishId, values.rating, values.author, values.message);
   }
 
   render(){
@@ -56,9 +56,9 @@ class CommentForm extends Component{
                         </Col>
             </Row>
             <Row className="form-group">
-                        <Label htmlFor="yourname" md={4}>Your Name</Label>
+                        <Label htmlFor="author" md={4}>Your Name</Label>
                         <Col md={12}>
-                            <Control.text model=".yourname" id="yourname" name="yourname"
+                            <Control.text model=".author" id="author" name="author"
                                 placeholder="Your Name"
                                 className="form-control"
                                 validators = {{
@@ -66,7 +66,7 @@ class CommentForm extends Component{
                                 }} />
                                 <Errors
                                 className="text-danger"
-                                model=".yourname"
+                                model=".author"
                                 show="touched"
                                 messages = {{
                                   required: 'Required ',
@@ -96,7 +96,7 @@ class CommentForm extends Component{
 }
 
 
-  function RenderComments({comments}){
+  function RenderComments({comments, addComment, dishId}){
     const comment = comments.map( (comment) => {
       return (
         <ul class = "list-unstyled" key={comment.id}>
@@ -113,7 +113,7 @@ class CommentForm extends Component{
           <CardBody>
             <CardTitle><h4><b>Comments</b></h4></CardTitle>
             <CardText>{comment}</CardText>
-            <CommentForm />
+            <CommentForm dishId = {dishId} addComment = {addComment} />
           </CardBody>
           </Card>
         </div>
@@ -162,7 +162,9 @@ const DishDetail = (props) => {
                     <RenderDish dish={props.dish} />
                   </div>
                   <div  className="col-12 col-md-5 m-1">
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments}
+                    addComment = {props.addComment}
+                    dishId = {props.dish.id} />
                   </div>
               </div>
     </div>
